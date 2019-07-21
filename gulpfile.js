e881ad55ba0c107a1f66fe73fb9052543ce6b4e5
Jordan Hlebarov <jd@hlebarov.com>
2019-07-21 16:38:45 +0300

    sass

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// Include modules
+var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	sass = require('gulp-sass');
+	cleanCSS = require ('gulp-clean-css');
+	sourcemaps = require('gulp-sourcemaps');
+	autoprefixer = require('gulp-autoprefixer');
+
+// Configure BrowserSync
+gulp.task('browser-sync', function() {
+	var files = [
+		'./style.css',
+		'./assets/css/enthusiast.css',
+		'./assets/js/custom.js',
+		'./*.php',
+		'./**/*.php',
+		'./**/*.js'
+	];
+	
+	// Initiaze BrowserSync with Web server
+	browserSync.init(files, {
+		proxy: "https://2019-sinemorets.retreat.wordcamp.org"
+	});
+});
+
+// Configure Sass task
+gulp.task('sass', function() {
+	return gulp.src('./assets/scss/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sass({
+			'outputStyle': 'compressed'
+		}))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./assets/css'))
+		.pipe(browserSync.stream());
+});
+
+// Create the default watch task using 'gulp'
+
+gulp.task('default', ['sass', 'browser-sync'], function() {
+	gulp.watch("./assets/scss/**/*.scss", ['sass']);
+})
